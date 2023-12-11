9e135e592e02292985d96a9f5ff1f3091b8903c7
j4v3l <jj4v3l@gmail.com>
2023-12-11 01:42:56 -0500

    Update flake.nix files with descriptions and comments

--- default/flake.nix
+++ default/flake.nix
@@ -1,29 +1,39 @@
 {
+  # Description of the flake
   description = "Nixos config flake";
 
+  # Inputs for the flake
   inputs = {
+    # URL for the nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # Home-manager input, it's commented out
     # home-manager = {
     #   url = "github:nix-community/home-manager";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
+  # Outputs function, it's a function that takes several arguments
   outputs = { self, nixpkgs, ... }@inputs:
     let
+      # System architecture
       system = "x86_64-linux";
+      # Legacy packages for the system
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-    
+      # NixOS configurations
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            ./configuration.nix
-            # inputs.home-manager.nixosModules.default
-          ];
-        };
-
+        # Special arguments for the system
+        specialArgs = {inherit inputs;};
+        # Modules for the system
+        modules = [ 
+          # Path to the configuration.nix file
+          ./configuration.nix
+          # Home-manager module, it's commented out
+          # inputs.home-manager.nixosModules.default
+        ];
+      };
     };
 }
--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
 {
+  # Description of the flake
   description = "A simple flake with 1 template";
 
+  # Outputs function, it's a function that takes an argument named 'self'
   outputs = { self }: {
 
+    # Templates attribute, it's a set of templates
     templates = {
 
+      # Example template, it's a single template in the set
       example = {
+        # Path to the template
         path = ./default;
+        # Description of the template
         description = "Basic NixOS config flake";
       };
 
     };
 
+    # Default template, it's the template that will be used by default
     defaultTemplate = self.templates.example;
 
   };
